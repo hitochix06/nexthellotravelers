b42d198a014ebaf8f5b37d2f371ac711b0d4042d
hitochix06 <pachara.philibert@hotmail.fr>
2024-04-12 16:20:34 +0200

    Mise à jour des avantages de Next.js et de la section Hero

--- src/app/components/sections/Hero.js
+++ src/app/components/sections/Hero.js
@@ -1,27 +1,29 @@
 import React from "react";
 
 export default function Hero() {
-  return (
-    <div className="flex flex-row justify-between">
-      <div className="w-1/2">
-        <h1> Et si on apprenais à utiliser Next.js?</h1>
-        <p>
-          Aujourd'huit on va se servir de Next.js pour récupérer les acticles
-          d'un blog Wordpress de votre choix, pour les ajouter à notre site de
-          manière performante,sécurisée et SEO friendly
-        </p>
-        <div className="flex flex-row space-x-4">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Avantages de Next.js
-          </button>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Voir les articles
-          </button>
-        </div>
-      </div>
-      <div className="w-1/2">
-        <img src="./presentation.jpeg" alt="Votre image" />
-      </div>
+ return (
+  <div className="container mx-auto">
+   <div className="flex flex-col md:flex-row justify-center">
+    <div className="w-full md:w-1/2 px-20 py-5">
+     <h1 className="font-bold py-4" >  Et si on apprenais à utiliser Next.js?</h1>
+     <p className="text-gray-600">
+      Aujourd'hui on va se servir de Next.js pour récupérer les articles
+      d'un blog Wordpress de votre choix, pour les ajouter à notre site de
+      manière performante, sécurisée et SEO friendly
+     </p>
+     <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 py-4">
+      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+       Avantages de Next.js
+      </button>
+      <button className="border border-green-500 hover:border-green-700 text-green-500 hover:text-green-700 font-bold py-2 px-4 rounded">
+       Voir les articles
+      </button>
+     </div>
     </div>
-  );
+    <div className="w-full md:w-1/2 px-4 flex justify-center">
+     <img src="./presentation.jpeg" alt="Votre image" />
+    </div>
+   </div>
+  </div>
+ );
 }
