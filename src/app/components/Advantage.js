c2af3ca7de07f7a485f654b9796d5609d3238a3b
hitochix06 <pachara.philibert@hotmail.fr>
2024-04-12 17:36:12 +0200

    Mise à jour des avantages de Next.js et du composant Advantage

--- src/app/components/Advantage.js
+++ src/app/components/Advantage.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export default function Advantage() {
+export default function Advantage(props) {
  return (
   <div>
    <div className="container mx-auto">
     <div className="flex flex-col md:flex-row ">
      <div className="px-20">
-     <h2 className="font-bold py-4" >Next.js est un framework fantastique</h2>
+      <h4 className="font-bold py-4" >{props.title}</h4>
       <p className="text-gray-600">
-       Créez des applications web modernes, performantes et facilement maintenables sans forcer
-       </p>
+       {props.paragraphe}
+      </p>
      </div>
     </div>
    </div>
--- src/app/components/sections/Advantages.js
+++ src/app/components/sections/Advantages.js
@@ -1,10 +1,23 @@
 import Advantage from "../Advantage";
 import React from 'react'
+const advantages = require('../../../../data/advantages.json')
 
 export default function Advantages() {
  return (
   <div>
-   <Advantage />
+   <div className="container mx-auto">
+    <div className="flex flex-col md:flex-row ">
+     <div className="px-20">
+      <h2 className="font-bold py-4" >Next.js est un framework fantastique</h2>
+      <p className="text-gray-600">
+       Créez des applications web modernes, performantes et facilement maintenables sans forcer
+      </p>
+     </div>
+    </div>
+   </div>
+   {
+    advantages.map((el, i) => <Advantage {...el} key={i} />)
+   }
 
   </div>
  )
