256027458fedd4c2ef779beb82312c83a1f52526
hitochix06 <pachara.philibert@hotmail.fr>
2024-04-26 11:35:38 +0200

    Mise à jour des avantages et de la mise en page dans Advantages.js

--- src/app/components/Advantage.js
+++ src/app/components/Advantage.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Advantage(props) {
   return (
-    <div className="container mx-auto">
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 auto-rows-auto">
+    <div className="xs:col-span-12 sm:col-span-6 md:col-span-6  ">
+      <div className=" gap-4 auto-rows-auto">
         <div className="px-20 py-5">
           <div className="flex items-center">
             <img src={props.icon} alt="Icon" className="w-1/3 h-10 mr-4" />
