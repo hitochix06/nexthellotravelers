6dff57ff8b68d1c03b58ef1418ffa58a5b96c049
hitochix06 <pachara.philibert@hotmail.fr>
2024-04-26 11:40:02 +0200

    Mise à jour de la mise en page de l'avantage dans Advantage.js

--- src/app/components/Advantage.js
+++ src/app/components/Advantage.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Advantage(props) {
   return (
-    <div className="xs:col-span-12 sm:col-span-6 md:col-span-6  ">
+    <div className="xs:col-span-12 sm:col-span-6 md:col-span-6 lg:col-span-4 ">
       <div className=" gap-4 auto-rows-auto">
         <div className="px-20 py-5">
           <div className="flex items-center">
-            <img src={props.icon} alt="Icon" className="w-1/3 h-10 mr-4" />
+            <img src={props.icon} alt="Icon" className=" h-10 mr-4" />
             <h4 className="font-bold py-4">{props.title}</h4>
           </div>
           <p className="text-gray-600">{props.paragraphe}</p>
