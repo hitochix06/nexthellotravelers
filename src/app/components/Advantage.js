24c95932056a8d0e9d4a9962cfc7ed57d0b1768d
hitochix06 <pachara.philibert@hotmail.fr>
2024-04-26 10:32:42 +0200

    Mise à jour du composant Advantage dans Advantage.js

--- src/app/components/Advantage.js
+++ src/app/components/Advantage.js
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function Advantage(props) {
- return (
-  <div>
-   <div className="container mx-auto">
-    <div className="flex flex-col md:flex-row ">
-     <div className="px-20">
-      <h4 className="font-bold py-4" >{props.title}</h4>
-      <p className="text-gray-600">
-       {props.paragraphe}
-      </p>
-     </div>
+  return (
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 auto-rows-auto">
+        <div className="px-20 py-5">
+          <div className="flex items-center">
+            <img src={props.icon} alt="Icon" className="w-1/3 h-10 mr-4" />
+            <h4 className="font-bold py-4">{props.title}</h4>
+          </div>
+          <p className="text-gray-600">{props.paragraphe}</p>
+        </div>
+      </div>
     </div>
-   </div>
-  </div>
- )
+  );
 }
