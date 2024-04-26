9e7f02e5b7208ca1046c79a222a8c95e0c80c5a5
hitochix06 <pachara.philibert@hotmail.fr>
2024-04-26 12:47:35 +0200

    Mise à jour des classes de colonnes dans Advantage.js et suppression de lignes vides dans Articles.js

--- src/app/components/sections/Articles.js
+++ src/app/components/sections/Articles.js
@@ -1,8 +1,4 @@
 export default async function Articles() {
-
-
-
- 
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold">Articles</h1>
