99879d6f882a8242e507f7b5154b92fd925bbd0c
hitochix06 <pachara.philibert@hotmail.fr>
2024-05-03 12:25:55 +0200

    Mise à jour de la bannière - lien vers le portfolio et mise en page améliorée

--- src/app/components/sections/Banner.js
+++ src/app/components/sections/Banner.js
@@ -1,13 +1,19 @@
 export default function Banner() {
-    return (
-        <div className="hero py-10 bg-base-content text-black rounded-lg my-10">
-        <div className="hero-content text-center">
-          <div className="max-w-2xl">
-            <h1 className="text-5xl font-bold">Vous avez aimé cette démo ?</h1>
-            <p className="py-6 text-gray-400">Alors foncez voir ce qui se trouve sur mon portfolio</p>
-            <a href="https://pphilibert-web.eu/" target="blank" className="btn btn-white">Mon portfolio</a>
-          </div>
-        </div>
+  return (
+    <div className="hero py-10 bg-black text-white rounded-lg my-10 flex justify-center items-center text-center">
+      <div className="max-w-2xl p-8">
+        <h1 className="text-5xl font-bold">Vous avez aimé cette démo ?</h1>
+        <p className="py-6 text-gray-400">
+          Alors foncez voir ce qui se trouve sur mon portfolio
+        </p>
+        <a
+          href="https://pphilibert-web.eu/"
+          target="_blank"
+          className="text-sm font-semibold leading-6 text-white uppercase bg-gradient-to-r from-blue-500 to-blue-700 py-2 px-4 rounded-lg"
+        >
+          Mon portfolio<span aria-hidden="true"> →</span>
+        </a>
       </div>
-    )
+    </div>
+  );
 }
