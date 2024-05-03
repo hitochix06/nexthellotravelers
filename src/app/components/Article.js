f5a61247de360b01d430faa9c7ca563a49586e07
hitochix06 <pachara.philibert@hotmail.fr>
2024-05-03 10:03:32 +0200

    Mise à jour des composants Article, Titlesub, Articles et Footer

--- src/app/components/Article.js
+++ src/app/components/Article.js
@@ -1,1 +1,33 @@
 
+import Image from "next/image"
+import Link from "next/link"
+
+
+export default async function Article(props) {
+
+ const response = await fetch(`https://hellotravelersblog.com/wp-json/wp/v2/media/${props.featured_media}`)
+ const image = await response.json();
+ return (
+  <article className={`relative overflow-hidden rounded-lg shadow transition hover:shadow-lg ${props.classes}`}>
+   <img
+    fill
+    alt={`Image de ${props.title.rendered}`}
+    src={image.source_url}
+    className="absolute inset-0 h-full w-full object-cover"
+   />
+   <div className="relative bg-gradient-to-t from-gray-900/90 to-gray-900/30 h-full pt-32 sm:pt-48 lg:pt-64">
+    <div className="p-4 sm:p-6 h-full flex flex-col justify-end">
+     <p className="text-sm text-white/70">{new Date(props.date).toLocaleDateString()}</p>
+
+     <Link href={`/article/${props.slug}`} >
+      <h3 className="mt-0.5 text-lg text-white">{props.title.rendered}</h3>
+
+      <p className="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
+       {props.excerpt.rendered.replace(/<\/?[^>]+(>|$)/g, "")}
+      </p>
+     </Link>
+    </div>
+   </div>
+  </article>
+ )
+}
--- src/app/components/sections/Articles.js
+++ src/app/components/sections/Articles.js
@@ -2,6 +2,8 @@ import Article from "../Article";
 import Titlesub from "../Titlesub";
 
 export default async function Articles() {
+  const response = await fetch(`https://hellotravelersblog.com/wp-json/wp/v2/posts?per_page=6&_fields=id,slug,title,featured_media,date,excerpt`);
+  const articlesData = await response.json();
 
 
   return (
@@ -12,8 +14,11 @@ export default async function Articles() {
         classes="text-center flex flex-col items-center"
       />
       <div className="grid grid-cols-12 gap-8 mt-16 ">
-        
+        {articlesData.map((el, i) => <Article {...el} classes="col-span-12 sm:col-span-6 md:col-span-4" key={i} />)}
+
       </div>
     </div>
   );
 }
+
+
